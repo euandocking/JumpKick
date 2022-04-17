d4aab5ffdebf0e3bfb199756b23c7d86d2ed8aa3
euandocking <euandocking@hotmail.co.uk>
2022-04-17 19:05:47 +0100

    saving and loading of best times

--- scripts/Level.gd
+++ scripts/Level.gd
@@ -1,19 +1,33 @@
 extends Node2D
 
-func save(content):
-	var file = File.new()
-	file.open("user://save_game.dat", File.WRITE)
-	file.store_string(content)
-	file.close()
+var bestTime = -1
+export(String) var levelName
 
-func load():
-	var file = File.new()
-	file.open("user://save_game.dat", File.READ)
-	var content = file.get_as_text()
-	file.close()
-	return content
+func loadBestTime():
+	var save_game = File.new()
+	print(bestTime)
+	if save_game.file_exists("user:// " + levelName + ".save"):
+		save_game.open("user:// " + levelName + ".save", File.READ)
+		bestTime = save_game.get_float()
+		get_node("GUI/BestTime").set_text("Best Time: " + String(stepify(bestTime, 0.01)))
+		save_game.close()
+	print(bestTime)
 
 func win():
 	var time = get_node("GUI/PlayTime").time
+	
+	if bestTime == -1:
+		var save_game = File.new()
+		save_game.open("user:// " + levelName + ".save", File.WRITE)
+		save_game.store_float(time)
+		save_game.close()
+	elif time < bestTime:
+		var save_game = File.new()
+		save_game.open("user:// " + levelName + ".save", File.WRITE)
+		save_game.store_float(time)
+		save_game.close()
 	print("win: " + String(time) +  " seconds")
 	get_tree().change_scene("res://scenes/menus/LevelSelectMenu.tscn")
+
+func _ready():
+	loadBestTime()
--- scripts/LevelLoadButton.gd
+++ scripts/LevelLoadButton.gd
@@ -1,6 +1,26 @@
 extends "MenuButton.gd"
 
 export(String) var levelPath
+export(String) var levelName
+var bestTime = -1
+
+func loadBestTime():
+	var save_game = File.new()
+	if save_game.file_exists("user:// " + levelName + ".save"):
+		save_game.open("user:// " + levelName + ".save", File.READ)
+		bestTime = save_game.get_float()
+		save_game.close()
+	
+	var text = "Best Time: "
+	if bestTime > 0:
+		text += String(stepify(bestTime, 0.01))
+	else:
+		text += "-"
+	
+	get_node("BestTimeLabel").set_text(text)
+
+func _ready():
+	loadBestTime()
 
 func _on_MenuButton_pressed():
 	get_tree().change_scene(levelPath)
