94cf51b40e34292000bc755a960ceb784d256b28
euandocking <euandocking@hotmail.co.uk>
2022-04-18 03:13:06 +0100

    changed level start functionality

--- scripts/LevelGUI.gd
+++ scripts/LevelGUI.gd
@@ -1,6 +1,9 @@
 extends CanvasLayer
 
 func _process(delta):
+	if Input.is_action_just_pressed("restart"):
+		get_tree().reload_current_scene()
+	
 	if Input.is_action_just_pressed("exit"):
 		if get_node("MainMenu").visible:
 			get_node("MainMenu").hide()
--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -13,4 +13,9 @@ func _on_SettingsButton_pressed():
 
 
 func _on_QuitButton_pressed():
+	#save current level to file
+	var save_game = File.new()
+	save_game.open("user://startLevel.save", File.WRITE)
+	save_game.store_string(get_parent().get_parent().get_filename())
+	save_game.close()
 	get_tree().quit()
